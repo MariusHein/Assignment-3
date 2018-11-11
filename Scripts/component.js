f48736ddcc72c5dc6307172848323930a9a3f0b6
MariusHein <hein1994@web.de>
2018-11-11 12:43:51 +0100

    Fixed vulnerability

--- Scripts/component.js
+++ Scripts/component.js
@@ -2,7 +2,14 @@ function addComment() {
     // Get the input value
     var value = $('#inp').val();
 
-    // Vulnerable comment
-    $('#wrongDiv').append("<ul style='list-style-type: none' id='newWrongList'></ul>");
-    $("#newWrongList").append("<li>" + value + "</li>");
+    // Escaped comment
+    var ar = [];
+    ar.push(value);
+    var fixedValue = "";
+    $(ar).each(function (i, elm) {
+        var input = elm;
+        fixedValue += "<li>" + $("<div>").text(input).html() + "</li>\n";
+    });
+    $('#properDiv').append("<ul style='list-style-type: none' id='newProperDivList'></ul>");
+    $("#newProperDivList").append(fixedValue);
 }
